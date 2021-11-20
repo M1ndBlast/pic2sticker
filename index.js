c59aaabec95b93bddad6b648f845f43982fe3463
M1ndBlast <gustavpopdzz0@gmail.com>
2021-11-20 01:49:29 -0600

    fix condition jajaja

--- index.js
+++ index.js
@@ -40,32 +40,30 @@ client.on('disconnected', reason =>
 	info("LOG OUT "+reason));
 
 client.on('message', async msg => {
-	if (msg.from === "status@broadcast")
-		return
-
-	if (msg.body.toLocaleLowerCase() === 'help' || msg.body.toLocaleLowerCase() === 'ayuda')
-		await msg.reply('Manda una imagen o un video a esta conversación. \nSi deseas añadirle un titulo, solo tienes que agregarle un texto junto a la imagen/~video~ al enviarlo. \n\n_Cualquier problema presentado informalo al +525610338516_')
-	else if (msg.hasMedia) {
-		let mediaRes = undefined;
-		let options = { sendMediaAsSticker: true, stickerAuthor: "pic2sticker @m1ndblast"}
-		if (msg.type === MessageTypes.STICKER) {
-			mediaRes = new MessageMedia("image/webp", fs.readFileSync("stickers/sentsticker.webp", {encoding: "base64"}),"don't send stickers")
-			options.stickerName = mediaRes.filename
-			options.stickerCategories = ["😭"]
+	if (msg.from !== "status@broadcast")
+		if (msg.body.toLocaleLowerCase() === 'help' || msg.body.toLocaleLowerCase() === 'ayuda')
+			await msg.reply('Manda una imagen o un video a esta conversación. \nSi deseas añadirle un titulo, solo tienes que agregarle un texto junto a la imagen/~video~ al enviarlo. \n\n_Cualquier problema presentado informalo al +525610338516_')
+		else if (msg.hasMedia) {
+			let mediaRes = undefined;
+			let options = { sendMediaAsSticker: true, stickerAuthor: "pic2sticker @m1ndblast"}
+			if (msg.type === MessageTypes.STICKER) {
+				mediaRes = new MessageMedia("image/webp", fs.readFileSync("stickers/sentsticker.webp", {encoding: "base64"}),"don't send stickers")
+				options.stickerName = mediaRes.filename
+				options.stickerCategories = ["😭"]
+			}
+			else if (msg.type === MessageTypes.IMAGE) {
+				mediaRes = await msg.downloadMedia()
+				options.stickerName = mediaRes.filename?mediaRes.filename:msg.body
+				options.stickerCategories = ["🤣"]
+			}
+			else {
+				mediaRes = new MessageMedia("image/webp", fs.readFileSync("stickers/sentvideo.webp", {encoding: "base64"}),"unsupportable media")
+				options.stickerName = mediaRes.filename
+				options.stickerCategories = ["😭"]
+			}
+			if (mediaRes)
+				await msg.reply(mediaRes, null, options)
+			else
+				msg.reply("Huno un problema en la conversión de la imagen.\nMandar una captura de pantalla a https://wa.me/5215610338516 \n\n~No es un link externo, unicamente abre el chat dentro WhatsApp~")
 		}
-		else if (MessageTypes.IMAGE) {
-			mediaRes = await msg.downloadMedia()
-			options.stickerName = mediaRes.filename?mediaRes.filename:msg.body
-			options.stickerCategories = ["🤣"]
-		}
-		else {
-			mediaRes = new MessageMedia("image/webp", fs.readFileSync("stickers/sentvideo.webp", {encoding: "base64"}),"unsupportable media")
-			options.stickerName = mediaRes.filename
-			options.stickerCategories = ["😭"]
-		}
-		if (mediaRes)
-			await msg.reply(mediaRes, null, options)
-		else
-			msg.reply("Huno un problema en la conversión de la imagen.\nMandar una captura de pantalla a https://wa.me/5215610338516 \n\n~No es un link externo, unicamente abre el chat dentro WhatsApp~")
-	}
 })
